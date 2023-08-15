85a48c7bf3be4c0f9b1cabade9779f63c49d7dc6
Juz01 <jorgeuz01@hotmail.com>
2023-08-14 22:57:04 -0400

    adding supertest and jest also security to login

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,7 @@
 const catchError = require('../utils/catchError');
 const User = require('../models/User');
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const getAll = catchError(async(req, res) => {
     const results = await User.findAll();
@@ -20,6 +22,8 @@ const remove = catchError(async(req, res) => {
 
 const update = catchError(async(req, res) => {
     const { id } = req.params;
+    delete req.body.email
+    delete req.body.password
     const result = await User.update(
         req.body,
         { where: {id}, returning: true }
@@ -28,9 +32,48 @@ const update = catchError(async(req, res) => {
     return res.json(result[1][0]);
 });
 
+const verifyUser = catchError(async (req, res) => {
+    const { code } = req.params;
+    const emailCode = await emailCode.findOnce({ where: {code} })
+    if(!emailCode) return res.sendStatus(403)
+
+    const user = await User.update(
+        { isVerified: true },
+        { where: { id: emailCode.userId }, returning: true }
+    )
+
+    if(user[0] === 0) return res.sendStatus(404);
+
+    await emailCode.destroy()
+
+    return res.json(user[1][0])
+})
+
+const login = catchError(async(req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email } })
+    if(!user) return res.sendStatus(401)
+
+    const isValid = await bcrypt.compare(password, user.password)
+
+    if(!isValid) return res.sendStatus(401)
+
+    const token = jwt.sign(
+        {user},
+        process.env.TOKEN_SECRET,
+        {expiresIn: "1d"}
+    )
+
+    return res.json({ user, token })
+})
+
+
+
 module.exports = {
     getAll,
     create,
     remove,
-    update
+    update,
+    login,
+    verifyUser
 }
